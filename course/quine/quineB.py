451e4f2e570fddaa4e978282b38e9deeba25d97e
liuyang1 <liuyang1@mail.ustc.edu.cn>
2012-11-28 22:41:02 +0800

    daily

---
+++ course/quine/quineB.py
@@ -0,0 +1,13 @@
+# this program print out itself
+# by Python repr or `` func
+# 
+x='y="x="+`x`+"\\n"\nprint y+x'
+y="x="+`x`+"\n"
+print y+x
+
+print "x:----------------"
+print x
+print "------------------"
+print "y:----------------"
+print y
+print "------------------"
