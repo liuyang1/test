84666ddd44bc2f04cca35a56f073ec9491bb4abb
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-07-09 21:04:00 +0800

    update cavlc

--- compress/cavlc/cavlc.c
+++ compress/cavlc/cavlc.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include "tbl.h"
 
+#define BIT_SET(a, b)    ((a) |= (1 << (b)))
+#define BIT_CLEAR(a, b)  ((a) &= ~(1 << (b)))
+
 typedef int ErrorCode;
 
-int loopOnData(char* in, int len, int (*func)(char))
+int loopOnData(char* in, int len, int (* func)(char))
 {
     int s = 0;
-    for(int i=0; i < len; i++, in++)
+    for (int i = 0; i < len; i++, in++) {
         s += func(*in);
+    }
     return s;
 }
 
@@ -21,28 +25,28 @@ int totalCoeffs(char* in, int len)
     return loopOnData(in, len, NoZero);
 }
 
-int trailingsOnes(char *in, int len)
+int trailingsOnes(char* in, int len)
 {
     int s = 0;
-    in+=len-1;
-    for(int i=0; i < len; i++, in--){
-        if(*in == 0)
+    in += len - 1;
+    for (int i = 0; i < len; i++, in--) {
+        if (*in == 0)
             s++;
-        if(s == 3 || *in != 0)
+        if (s == 3 || *in != 0)
             return s;
     }
     return s;
 }
-int totalZeros(char *in, int len)
+int totalZeros(char* in, int len)
 {
     int s = 0;
-    for(int i=0; i < len; i++, in++){
-        if(*in == 0)
+    for (int i = 0; i < len; i++, in++) {
+        if (*in == 0)
             s++;
     }
     in--;
-    for(int i=0; i < len; i++, in--){
-        if(*in == 0)
+    for (int i = 0; i < len; i++, in--) {
+        if (*in == 0)
             s--;
         else
             break;
@@ -51,31 +55,65 @@ int totalZeros(char *in, int len)
 }
 void writeData(char* data, char c, int n)
 {
-    for(int i=0; i < n; i++, data++)
+    for (int i = 0; i < n; i++, data++) {
         *data = c;
+    }
 }
-        
+
+void writeBit(char* data, int bitoffset, int v)
+{
+    data += bitoffset / 8;
+    bitoffset %= 8;
+    bitoffset = 7 - bitoffset;
+    if (v > 0)
+        BIT_SET(*data, bitoffset);
+    else
+        BIT_CLEAR(*data, bitoffset);
+}
 ErrorCode cavlc(char* in, int insize, char* out, int outsize)
 {
-    int totalc=0, trail=0, totalz=0;
+    int totalc = 0, trail = 0, totalz = 0;
     totalc = totalCoeffs(in, insize);
     trail = trailingsOnes(in, insize);
-    totalz = totalZeros(in, insize);
-    printf("%d %d %d\n", totalc, trail, totalz);
     char buf[256];
     int bitoffset = writeCoeffToken(buf, 0, trail, totalc);
-    for(int i=0; i<bitoffset/8+1; i++)
+    char* p;
+    // search trailing ones flag
+    int i, cnt;
+    for (p = in + insize - 1, cnt = 0, i = 0; cnt < trail; i++, p--) {
+        switch ((int)*p) {
+        case 1:
+            cnt++;
+            writeBit(buf, bitoffset, 0);
+            bitoffset++;
+            break;
+        case -1:
+            cnt++;
+            writeBit(buf, bitoffset, 1);
+            bitoffset++;
+            break;
+        default:
+            break;
+        }
+    }
+    // levels encoding
+    for (; i < insize; i++, p--) {
+        if (*p == 0) {} else  {}
+    }
+    printf("bitoffset: %d\n", bitoffset);
+    for (int i = 0; i < bitoffset / 8 + 1; i++) {
         printf("0x%02x\n", buf[i]);
-    printf("bitoffset %d\n", bitoffset);
+    }
+    totalz = totalZeros(in, insize);
     return 0;
 }
 
 void test()
 {
     char a[] = {0, 3, 0, 1, -1,
-        -1, 0, 1, 0, 0,
-        0, 0, 0, 0, 0,
-        0};
+                -1, 0, 1, 0, 0,
+                0, 0, 0, 0, 0,
+                0};
     int len = sizeof(a) / sizeof(a[0]);
     char buf[256];
     cavlc(a, len, buf, 256);
--- compress/cavlc/tbl.c
+++ compress/cavlc/tbl.c
@@ -45,6 +45,76 @@ char NC0_CoeffTokenTbl[TOTALCOEFF_MAX * TRAILINGSONES_MAX * TOKENSIZE] =
     11,     0,              0b11000000,
     10,     0b00000001,     0b01000000,
     8,      0b00000100,     0,
+
+    13,     0,              0b01011000,
+    13,     0,              0b01110000,
+    11,     0,              0b10100000,
+    9,      0b00000010,     0,
+
+    13,     0,              0b01000000,
+    13,     0,              0b01010000,
+    13,     0,              0b01101000,
+    10,     0b00000001,     0,
+
+    14,     0,              0b00111100,
+    14,     0,              0b00111000,
+    13,     0,              0b01001000,
+    11,     0,              0b10000000,
+
+    14,     0,              0b00101100,
+    14,     0,              0b00101000,
+    14,     0,              0b00110100,
+    13,     0,              0b01100000,
+
+    15,     0,              0b00011110,
+    15,     0,              0b00011100,
+    14,     0,              0b00100100,
+    14,     0,              0b00110000,
+
+    15,     0,              0b00010110,
+    15,     0,              0b00010100,
+    15,     0,              0b00011010,
+    14,     0,              0b00100000,
+
+    16,     0,              0b00001111,
+    15,     0,              0b00000010,
+    15,     0,              0b00010010,
+    15,     0,              0b00011000,
+
+    16,     0,              0b00001011,
+    16,     0,              0b00001110,
+    16,     0,              0b00001101,
+    15,     0,              0b00010000,
+
+    16,     0,              0b00000111,
+    16,     0,              0b00001010,
+    16,     0,              0b00001001,
+    16,     0,              0b00001100,
+
+    16,     0,              0b00000100,
+    16,     0,              0b00000110,
+    16,     0,              0b00000101,
+    16,     0,              0b00001000,
+};
+
+char NC2_CoeffTokenTbl[TOTALCOEFF_MAX * TRAILINGSONES_MAX * TOKENSIZE] =
+{
+};
+
+char NC4_CoeffTokenTbl[TOTALCOEFF_MAX * TRAILINGSONES_MAX * TOKENSIZE] =
+{
+};
+
+char NC8_CoeffTokenTbl[TOTALCOEFF_MAX * TRAILINGSONES_MAX * TOKENSIZE] =
+{
+};
+
+char NC_1_CoeffTokenTbl[TOTALCOEFF_MAX * TRAILINGSONES_MAX * TOKENSIZE] =
+{
+};
+
+char NC_2_CoeffTokenTbl[TOTALCOEFF_MAX * TRAILINGSONES_MAX * TOKENSIZE] =
+{
 };
 
 char*checkNC2Tbl(int nc)
@@ -55,12 +125,13 @@ char*checkNC2Tbl(int nc)
     } else if (nc > 8)
         nc = 8;
     char* tbl[11] = {
-        NULL,                                   // NC = -2
-        NULL,                                   // NC = -1
-        NC0_CoeffTokenTbl, NC0_CoeffTokenTbl,   // 0 <= NC < 2
-        NULL, NULL,                             // 2 <= NC < 4
-        NULL, NULL, NULL, NULL,                 // 4 <= NC < 8
-        NULL                                    // 8 <= NC
+        NC_2_CoeffTokenTbl,                                     // NC = -2
+        NC_1_CoeffTokenTbl,                                     // NC = -1
+        NC0_CoeffTokenTbl, NC0_CoeffTokenTbl,                   // 0 <= NC < 2
+        NC2_CoeffTokenTbl, NC2_CoeffTokenTbl,                   // 2 <= NC < 4
+        // 4 <= NC < 8
+        NC4_CoeffTokenTbl, NC4_CoeffTokenTbl, NC4_CoeffTokenTbl, NC4_CoeffTokenTbl,
+        NC8_CoeffTokenTbl                                       // 8 <= NC
     };
     return tbl[nc + 2];
 }
