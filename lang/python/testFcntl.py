69b54cf9e231e7f3ac1a05a9aafd6ef4fd3abc40
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-11-27 19:20:53 +0800

    update

--- lang/python/testFcntl.py
+++ lang/python/testFcntl.py
@@ -1,15 +1,18 @@
 #!/bin/python
-import time,os,signal
+import os
+import signal
 from fcntl import *
 
-FNAME="."
+FNAME = "."
 
-def handler(signum,frame):
-	print "File %s modified" %(FNAME,)
 
-signal.signal(signal.SIGIO,handler)
-fd=os.open(FNAME,os.O_RDONLY)
-fcntl(fd,F_SETSIG,0)
-fcntl(fd,F_NOTIFY,DN_MODIFY|DN_CREATE|DN_MULTISHOT)
+def handler(signum, frame):
+    print "File %s modified" % (FNAME,),
+    print signum, frame
+
+signal.signal(signal.SIGIO, handler)
+fd = os.open(FNAME, os.O_RDONLY)
+fcntl(fd, F_SETSIG, 0)
+fcntl(fd, F_NOTIFY, DN_MODIFY | DN_CREATE | DN_MULTISHOT)
 while True:
-	pass
+    pass
