7ec6cef9b07c84febc87dc101cfc88f02d6c7013
liuyang1 <liuyang1@mail.ustc.edu.cn>
2015-02-01 20:57:39 +0800

    update 509.2.py

--- problemset/509.2.py
+++ problemset/509.2.py
@@ -1,6 +1,6 @@
-# WRONG ANSWER on test 4
+import copy
 import sys
-def shuru():
+def inputData():
     s = input()
     n, k = [int(i) for i in s.split()]
     s = input()
@@ -8,53 +8,32 @@ def shuru():
     return n, k, a
 
 
-def prt(s):
-    ss = ""
-    for idx, i in sorted(s.items(), key=lambda x: x[0]):
-        if i == 0:
-            continue
-        o = " ".join(["%d" % (idx + 1) for j in range(i)])
-        if ss != "":
-            ss += " " + o
-        else:
-            ss = o
-    ss += "\n"
-    return ss
-
+def isOK(a, k):
+    return max(a) - min(a) <= k
 
 def build(a, k):
-    a0 = min(a[0], k)
-    s = {}
-    for i in range(k):
-        if i < a0:
-            s[i] = 1
-        else:
-            s[i] = 0
-    o = prt(s)
-    for i in range(len(a) - 1):
-        d = abs(a[i + 1] - a[i])
-        if a[i + 1] > a[i]:
-            sign = 1
-            rev = False
-        else:
-            sign = -1
-            rev = True
-        cnt = 0
-        for idx, i in sorted(s.items(), key=lambda x: x[1], reverse=rev):
-            if i == 0 and sign < 0:
-                continue
-            if cnt == d:
-                break
-            i = i + sign
-            s[idx] = i
-            cnt += 1
-        if cnt != d:
-            print("NO")
-            return
-        o += prt(s)
-    print("YES")
-    print(o, end="")
+    a0 = min(a)
+    d = {}
+    d[a0] = [(0, a0)]
+    lst = [(0, a0 + 1)]
+    d[a0 + 1] = lst
+    color = 1
+    for i in range(a0 + 2, max(a) + 1):
+        lst = copy.deepcopy(lst)
+        lst.append((color, 1))
+        color += 1
+        d[i] = lst
+    for i in a:
+        l = d[i]
+        for c, n in l:
+            for i in range(n):
+                print(c + 1, end=" ")
+        print()
 
 if __name__ == "__main__":
-    n, k, a = shuru()
-    build(a, k)
+    n, k, a = inputData()
+    if not isOK(a, k):
+        print("NO")
+    else:
+        print("YES")
+        build(a, k)
