64ebb83a11f15a6693bc05b6a55cdd799b3b5f61
liuyang1 <liuyang1@mail.ustc.edu.cn>
2015-07-18 08:50:22 +0800

    add visitor @ python

---
+++ lang/python/vistor.py
@@ -0,0 +1,61 @@
+# Visitor/FlowerVisitors.py
+# Demonstration of "visitor" pattern.
+from __future__ import generators
+import random
+
+# The Flower hierarchy cannot be changed:
+class Flower(object):
+    ### this is key of visitor pattern ###
+    # Flower's accept method, to accept a visitor
+    # and let visitor do sth on me
+    def accept(self, visitor):
+        visitor.visit(self)
+    def pollinate(self, pollinator):
+        print(self, "pollinated by", pollinator)
+    def eat(self, eater):
+        print(self, "eaten by", eater)
+    def __str__(self):
+        return self.__class__.__name__
+
+class Gladiolus(Flower): pass
+class Runuculus(Flower): pass
+class Chrysanthemum(Flower): pass
+
+class Visitor:
+    def __str__(self):
+        return self.__class__.__name__
+
+class Bug(Visitor): pass
+class Pollinator(Bug): pass
+class Predator(Bug): pass
+
+# Add the ability to do "Bee" activities:
+# vistor have ability, and determine its logic to access Flower.
+class Bee(Pollinator):
+    def visit(self, flower):
+        flower.pollinate(self)
+
+# Add the ability to do "Fly" activities:
+class Fly(Pollinator):
+    def visit(self, flower):
+        flower.pollinate(self)
+
+# Add the ability to do "Worm" activities:
+class Worm(Predator):
+    def visit(self, flower):
+        flower.eat(self)
+
+def flowerGen(n):
+    flwrs = Flower.__subclasses__()
+    for i in range(n):
+        yield random.choice(flwrs)()
+
+# It's almost as if I had a method to Perform
+# various "Bug" operations on all Flowers:
+bee = Bee()
+fly = Fly()
+worm = Worm()
+for flower in flowerGen(3):
+    flower.accept(bee)
+    flower.accept(fly)
+    flower.accept(worm)
