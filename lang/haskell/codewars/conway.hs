837bd2a1b120ced8e1772f2200c588762b9e4532
liuyang1 <liuyang1@mail.ustc.edu.cn>
2015-07-19 23:53:14 +0800

    update fix-point combinator @ haskell

--- lang/haskell/codewars/conway.hs
+++ lang/haskell/codewars/conway.hs
@@ -4,7 +4,7 @@ import Control.Arrow
 
 digit n xs base = if n == 0 then xs else digit (n `div` base) ((n `mod` base):xs) base
 digit10 n = if n == 0 then [0] else digit n [] 10
-num = foldl (\x y -> 10 * x + y) 0
+num = foldl1 (\x y -> 10 * x + y)
 
 lookSay :: Integer -> Integer
 lookSay n = num . concatMap (\x -> [toInteger (length x), head x]) . group $ digit10 n
---
+++ lang/haskell/codewars/fix.hs
@@ -0,0 +1,16 @@
+module Fixit where
+import Prelude hiding (reverse, foldr)
+
+fix :: (a -> a) -> a
+fix f = let x = f x in x
+
+reverse' :: ([a] -> [a]) -> [a] -> [a]
+reverse' f [] = []
+reverse' f (a:as) = f as ++ [a]
+
+foldr' :: ((b -> a -> b) -> b -> [a] -> b) -> (b -> a -> b) -> b -> [a] -> b
+foldr' f fn d [] = d
+foldr' f fn d (x:xs) = fn (f fn d xs) x
+
+fixrev = fix reverse'
+fixfoldr = fix foldr'
