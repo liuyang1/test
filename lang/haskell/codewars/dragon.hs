aab177756ac24ff6246534baa3bdfbba60d2c25d
liuyang1 <liuyang1@mail.ustc.edu.cn>
2015-08-03 20:15:49 +0800

    update dragon

--- lang/haskell/codewars/dragon.hs
+++ lang/haskell/codewars/dragon.hs
@@ -1,11 +1,17 @@
 module DragonCurve where
 import Data.Char
 
-iter [] = []
-iter ('a':xs) = "aRbFR" ++ iter xs
-iter ('b':xs) = "LFaLb" ++ iter xs
-iter (x:xs) = x: iter xs
+iter 'a' = "aRbFR"
+iter 'b' = "LFaLb"
+iter x = [x]
 
 dragon n
   | n < 0 = ""
-  | otherwise = filter isUpper $ (iterate iter "Fa") !! n
+  | otherwise = filter isUpper $ (iterate (>>= iter) "Fa") !! n
+
+-- generate logo script as dragon cruve
+genLogo 'F' = "FD 6\n"
+genLogo 'L' = "LT 90\n"
+genLogo 'R' = "RT 90\n"
+
+main = putStr $ (dragon 10 >>= genLogo)
