fb604eb5b908c3c9fa8788d9937314de5103c54b
liuyang1 <liuyang1@mail.ustc.edu.cn>
2015-11-05 15:29:13 +0800

    C: designator

--- lang/c/testArrayInit.c
+++ lang/c/testArrayInit.c
@@ -1,7 +1,22 @@
 #include "stdio.h"
-int main(int argc, char *argv[])
+
+int main(void)
 {
-    int a[] = {[0] = 1, [2]= 3};
-    printf("%d %d %d\n", a[0], a[1], a[2]);
+    const char *style;
+
+    style = "index style";
+    int a0[] = {[0] = 1, 2, [2]= 3};
+    // each initializer element taht does not have a designator applies to the
+    // next consecutive element of the arrya or struct.
+    // So 2 init to index 1 as it after index 0
+    // ref:[[https://gcc.gnu.org/onlinedocs/gcc-4.4.1/gcc/Designated-Inits.html#Designated-Inits]]
+    printf("%s: %d %d %d\n", style, a0[0], a0[1], a0[2]);
+
+    style = "range style, [first ... last] = value";
+    int a1[] = {[0 ... 1] = 1, [2] = 3};
+    printf("%s: %d %d %d\n", style, a1[0], a1[1], a1[2]);
+
+    // index also can use charcters or `enum` type.
+
     return 0;
 }
--- lang/c/testStructInit.c
+++ lang/c/testStructInit.c
@@ -21,6 +21,14 @@ int main(void)
     memset(&v2, 0xaa, sizeof(struct st));
     printf("%s: %d(%x) %d(%x)\n", style, v2.a, v2.a, v2.b, v2.b);
 
+    // obsolete since GCC 2.5
+    // This is GNU old-style field designator extension, NOT use it.
+    // This designator style, also can EMBEDED LEVEL.
+    // ref:[[https://gcc.gnu.org/onlinedocs/gcc-4.4.1/gcc/Designated-Inits.html#Designated-Inits]]
+    style = "field name style";
+    struct st v3 = {b: 2, a:1};
+    printf("%s: %d %d\n", style, v3.a, v3.b);
+
     printf("Actually only GCC-EXTEND style and STD style are struct INIT.\n");
     return 0;
 }
