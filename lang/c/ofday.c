75efa35ff69e1d6d45f98e41d589858f51d6580f
liuyang1 <liuyang1@mail.ustc.edu.cn>
2015-11-10 10:32:23 +0800

    linux: time: not use gettimeofday to measure time.

--- lang/c/ofday.c
+++ lang/c/ofday.c
@@ -3,14 +3,23 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-int main(){
-	struct timeval t;
-	struct timezone tz;
-	gettimeofday(&t,&tz);
-	time_t now;
-	time(&now);
-	printf("time : %ld sec\n",now);
-	printf("timeodday : %ld . %ld sec\n",t.tv_sec,t.tv_usec);
-	printf("timezone : %ld min\n",tz.tz_minuteswest);
-	return 0;
+int main() {
+    struct timeval t;
+    struct timezone tz;
+    gettimeofday(&t, &tz);
+    time_t now;
+    time(&now);
+    printf("time : %ld sec\n", now);
+    printf("timeodday : %lu . %lu sec sizeof(sec)=%lu\n",
+           t.tv_sec, t.tv_usec,
+           sizeof(t.tv_sec));
+    printf("timezone : %u min\n", tz.tz_minuteswest);
+
+    // convert time to 32bit test.
+    unsigned long long ms = t.tv_sec * 1000 + t.tv_usec / 1000;
+    printf("ms: %llu to U32 %u dt=%u\n",
+           ms, (unsigned int)(ms), (unsigned int)(-1) - (unsigned int)(ms));
+    unsigned int dt = (unsigned int)(-1) - (unsigned int)(ms);
+    printf("dt=%u %d\n", dt, dt / 1000 / 60 / 60 / 24);
+    return 0;
 }
---
+++ lang/c/testFakeTime.c
@@ -0,0 +1,51 @@
+/*
+ * This is POC of change time in program, to get fake gettimeofday result.
+ * This program need ROOT priveldge, or CAP_SYS_TIME.
+ * Make sure not run this program in KEY server.
+ */
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+void longtimefunc()
+{
+    sleep(3);
+}
+
+void moveTime(int sec)
+{
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    now.tv_sec += sec;
+    int ret = settimeofday(&now, NULL);
+    if (ret != 0) {
+        printf("settimeofday: ret=%d\n", ret);
+        perror("settimeofday:");
+    }
+}
+
+#define TIC { \
+    struct timeval start,stop; \
+    gettimeofday(&start,NULL);
+
+#define TOC gettimeofday(&stop,NULL); \
+    printf("%lu sec %luusec\n", \
+           stop.tv_sec - start.tv_sec, stop.tv_usec -start.tv_usec); \
+    } \
+
+int main()
+{
+    // Warning: if settime, must set it back. Else get chaos of time.
+    int sec = -1;
+    TIC;
+    moveTime(sec);
+    longtimefunc();
+    TOC;
+
+    moveTime(-1 * sec);
+
+    return 0;
+}
