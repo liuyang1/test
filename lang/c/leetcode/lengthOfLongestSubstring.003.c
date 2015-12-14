f0a28498b1129a08d68a2c494ef0fd02984c0d4d
liuyang1 <liuyang1@mail.ustc.edu.cn>
2015-12-14 20:27:02 +0800

    C: leetcode: lengthOfLongestSubstring

---
+++ lang/c/leetcode/lengthOfLongestSubstring.003.c
@@ -0,0 +1,33 @@
+int lengthOfLongestSubstring(char* s) {
+    int i, len = strlen(s);
+    if (len <= 1) {
+        return len;
+    }
+    bool *aIsRepeat = (bool *)malloc(sizeof(bool) * len);
+    for (i = 0; i != len; i++) {
+        aIsRepeat[i] = false;
+    }
+
+    int thresh = 1;
+    bool find = true;
+    // Only IF find a[i, i+t] diff, and t != len, THEN continue find
+    while (find && thresh != len) {
+        for (i = 0, find = false; i != len - thresh; i++) {
+            if (aIsRepeat[i] == true) {
+                continue;
+            }
+            // IF a[i, i+t] diff AND a[i+1, i+t+1] diff AND a[i], a[i+t] diff,
+            //      THEN, a[i, i+t+1] diff.
+            if (aIsRepeat[i + 1] == true || s[i] == s[i + thresh]) {
+                aIsRepeat[i] = true;
+            } else if (find == false) {
+                find = true;
+            }
+        }
+        if (find) {
+            thresh++;
+        }
+    }
+    free(aIsRepeat);
+    return thresh;
+}
