a1ccf0e2fde8f21a5fca808acde5ad075ef7aa01
liuyang1 <liuyang1@mail.ustc.edu.cn>
2016-03-17 09:10:28 +0800

    C: leetcode: InterleaveStr

--- lang/c/leetcode/interlaveStr.097.c
+++ lang/c/leetcode/interlaveStr.097.c
@@ -1,31 +1,59 @@
 #include "leet.h"
 
-// BAD performance as exponentail complexity
-inline bool isInterleaveI(char *s1, char *s2, char *s3) {
-    if (*s3 == '\0') {
+// should different with false 0 or true 1
+#define UNDEF 2
+
+bool isInterleaveI(int *mem, int m, int n, char *s1, char *s2, char *s3,
+                   int csr1, int csr2);
+bool isInterleaveM(int *mem, int m, int n, char *s1, char *s2, char *s3,
+                   int csr1, int csr2) {
+    int csr = csr1 * n + csr2;
+    if (mem[csr] == UNDEF) {
+        mem[csr] = isInterleaveI(mem, m, n, s1, s2, s3, csr1, csr2);
+    }
+    return mem[csr];
+}
+
+bool isInterleaveI(int *mem, int m, int n, char *s1, char *s2, char *s3,
+                   int csr1, int csr2) {
+    char c3 = s3[csr1 + csr2];
+    if (c3 == '\0') {
         return true;
     }
-    if (*s3 == *s1 && *s3 == *s2) {
-        return isInterleaveI(s1 + 1, s2, s3 + 1) ||
-               isInterleaveI(s1, s2 + 1, s3 + 1);
-    } else if (*s3 == *s1) {
-        return isInterleaveI(s1 + 1, s2, s3 + 1);
-    } else if (*s3 == *s2) {
-        return isInterleaveI(s1, s2 + 1, s3 + 1);
+    bool b1 = c3 == s1[csr1];
+    bool b2 = c3 == s2[csr2];
+    if (b1 && b2) {
+        return isInterleaveM(mem, m, n, s1, s2, s3, csr1 + 1, csr2) ||
+               isInterleaveM(mem, m, n, s1, s2, s3, csr1, csr2 + 1);
+    } else if (b1) {
+        return isInterleaveM(mem, m, n, s1, s2, s3, csr1 + 1, csr2);
+    } else if (b2) {
+        return isInterleaveM(mem, m, n, s1, s2, s3, csr1, csr2 + 1);
     } else {
         return false;
     }
 }
 
 bool isInterleave(char *s1, char *s2, char *s3) {
-    if (strlen(s1) + strlen(s2) != strlen(s3)) {
+    int l1 = strlen(s1);
+    int l2 = strlen(s2);
+    int l3 = strlen(s3);
+    if (l1 + l2 != l3) {
         return false;
     }
-    return isInterleaveI(s1, s2, s3);
+    int sz = (l1 + 1) * (l2 + 1);
+    int *mem = malloc(sizeof(int) * sz);
+    int i;
+    for (i = 0; i != sz; i++) {
+        mem[i] = UNDEF;
+    }
+    bool r = isInterleaveM(mem, l1 + 1, l2 + 1, s1, s2, s3, 0, 0);
+    free(mem);
+    return r;
 }
 
 #define CASE(s1, s2, s3, e) {bool r = isInterleave(s1, s2, s3); \
-                             printf("%s %s %s\n%s ?= %s %s\n", \
+                             printf("[%s] [%s] [%s]\n%s ?= %s %s\n", \
                                     s1, s2, s3, SBOOL(r), SBOOL(e), expect(r == e)); }
 int main() {
     CASE("", "", "", true);
