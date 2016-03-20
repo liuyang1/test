aab3edd9ea9023d054a6e4d070f19ee8e6690842
liuyang1 <liuyang1@mail.ustc.edu.cn>
2016-03-20 22:18:30 +0800

    algs4: wordnet: WordNet, SAP

--- course/algs4/01_WordNet/SAP.java
+++ course/algs4/01_WordNet/SAP.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.StdIn;
@@ -5,44 +7,114 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
 
-   // constructor takes a digraph (not necessarily a DAG)
-   public SAP(Digraph G) {
-   }
+    Digraph mG;
+    // constructor takes a digraph (not necessarily a DAG)
+    public SAP(Digraph G) {
+        mG = G;
+    }
 
-   // length of shortest ancestral path between v and w; -1 if no such path
-   public int length(int v, int w) {
-       return -1;
-   }
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w) {
+        LinkedList<Integer> vs = new LinkedList<Integer>();
+        vs.add(v);
+        LinkedList<Integer> ws = new LinkedList<Integer>();
+        ws.add(w);
+        return length(vs, ws);
+    }
 
-   // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-   public int ancestor(int v, int w) {
-       return -1;
-   }
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w) {
+        LinkedList<Integer> vs = new LinkedList<Integer>();
+        vs.add(v);
+        LinkedList<Integer> ws = new LinkedList<Integer>();
+        ws.add(w);
+        return ancestor(vs, ws);
+    }
 
-   // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-   public int length(Iterable<Integer> v, Iterable<Integer> w) {
-       return -1;
-   }
+    private LinkedList<Integer> adj(LinkedList<Integer> v) {
+        LinkedList<Integer> lst = new LinkedList<Integer>();
+        for (int vv: v) {
+            for (int w: mG.adj(vv)) {
+                lst.add(w);
+            }
+        }
+        return lst;
+    }
+    private final static int NOTFIND = -1;
+    private int findAncestor(Iterable<Integer> v, Iterable<Integer> w) {
+        for (int a: v) {
+            for (int b: w) {
+                if (a == b) {
+                    return a;
+                }
+            }
+        }
+        return NOTFIND;
+    }
+    private class Result {
+        int mStep;
+        int mAncestor;
+        Result(int step, int ancestor) {
+            mStep = step;
+            mAncestor = ancestor;
+        }
+    }
+    private Result search(LinkedList<Integer> v, LinkedList<Integer> w) {
+        int step = 0;
+        int size = mG.V();
+        ArrayList< LinkedList<Integer> > vs = new ArrayList< LinkedList<Integer> >();
+        ArrayList< LinkedList<Integer> > ws = new ArrayList< LinkedList<Integer> >();
+        vs.add(v);
+        ws.add(w);
+        int i;
+        for (; step <= size; step++) {
+            for (i = 0; i != step + 1; i++) {
+                v = vs.get(i);
+                w = ws.get(step - i);
+                int r = findAncestor(v, w);
+                if (r != NOTFIND) {
+                    return new Result(step, r);
+                }
+            }
+            vs.add(adj(vs.get(step)));
+            ws.add(adj(ws.get(step)));
+        }
+        return new Result(-1, NOTFIND);
+    }
+    private LinkedList<Integer> trans(Iterable<Integer> lst) {
+        LinkedList<Integer> ret = new LinkedList<Integer>();
+        for (int i: lst) {
+            ret.add(i);
+        }
+        return ret;
+    }
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        Result ret = search(trans(v), trans(w));
+        return ret.mStep;
+    }
 
-   // a common ancestor that participates in shortest ancestral path; -1 if no such path
-   public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-       return -1;
-   }
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        Result ret = search(trans(v), trans(w));
+        return ret.mAncestor;
+    }
 
-   public static void testCase(String[] args) {
-       In in = new In(args[0]);
-       Digraph G = new Digraph(in);
-       SAP sap = new SAP(G);
-       while (!StdIn.isEmpty()) {
-           int v = StdIn.readInt();
-           int w = StdIn.readInt();
-           int length   = sap.length(v, w);
-           int ancestor = sap.ancestor(v, w);
-           StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
-       }
-   }
-   // do unit testing of this class
-   public static void main(String[] args) {
-       return;
-   }
+    public static void testCase(String fnDigraph) {
+        In in = new In(fnDigraph);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length   = sap.length(v, w);
+            int ancestor = sap.ancestor(v, w);
+            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+        }
+    }
+    // do unit testing of this class
+    public static void main(String[] args) {
+        testCase("test/wordnet/digraph1.txt");
+        return;
+    }
 }
--- course/algs4/01_WordNet/WordNet.java
+++ course/algs4/01_WordNet/WordNet.java
@@ -1,31 +1,113 @@
+import edu.princeton.cs.algs4.Digraph;
+import java.util.*;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class WordNet {
+    private List < String[] > loadCSV(String csv) {
+        File f = new File(csv);
+        List< String[] > lst = new LinkedList< String[] >();
+        String line;
+        BufferedReader rd = null;
+        try {
+            rd = new BufferedReader(new FileReader(f));
+            while ((line = rd.readLine()) != null) {
+                String[] item = line.split(",");
+                lst.add(item);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (rd != null) {
+                try {
+                    rd.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return lst;
+    }
+    private void showCSV(List< String[] > lst) {
+        for (String[] item: lst) {
+            for (String one: item) {
+                System.out.printf("%s ", one);
+            }
+            System.out.printf("\n");
+        }
+    }
+    Digraph mG;
+    HashMap<Integer, String> mTblSyn;
+    SAP mSap;
+    // constructor takes the name of the two input files
+    public WordNet(String synsets, String hypernyms) {
+        List< String[] > lstSyn = loadCSV(synsets);
+        mTblSyn = new HashMap<Integer, String>();
+        for (String[] item: lstSyn) {
+            mTblSyn.put(Integer.parseInt(item[0]), item[1]);
+        }
 
-   // constructor takes the name of the two input files
-   public WordNet(String synsets, String hypernyms) {
-   }
+        List< String[] > lstHyp = loadCSV(hypernyms);
+        mG = new Digraph(mTblSyn.size());
+        for (String[] item: lstHyp) {
+            int begin = Integer.parseInt(item[0]), end;
+            int i;
+            for (i = 1; i != item.length; i++) {
+                end = Integer.parseInt(item[i]);
+                mG.addEdge(begin, end);
+            }
+        }
+        System.out.printf("mG=%s\n", mG);
+        mSap = new SAP(mG);
+    }
 
-   // returns all WordNet nouns
-   public Iterable<String> nouns() {
-       return null;
-   }
+    // returns all WordNet nouns
+    public Iterable<String> nouns() {
+        return mTblSyn.values();
+    }
 
-   // is the word a WordNet noun?
-   public boolean isNoun(String word) {
-       return false;
-   }
+    // is the word a WordNet noun?
+    public boolean isNoun(String word) {
+        for (String s: nouns()) {
+            if (s.equals(word)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-   // distance between nounA and nounB (defined below)
-   public int distance(String nounA, String nounB) {
-       return -1;
-   }
+    // distance between nounA and nounB
+    public int distance(String nounA, String nounB) {
+        return -1;
+    }
 
-   // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-   // in a shortest ancestral path (defined below)
-   public String sap(String nounA, String nounB) {
-       return null;
-   }
+    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // in a shortest ancestral path
+    public String sap(String nounA, String nounB) {
+        return null;
+    }
 
-   // do unit testing of this class
-   public static void main(String[] args) {
-   }
+    private static void test() {
+        String path = "test/wordnet";
+        String synsets = path + "/synsets11.txt";
+        String hypernyms = path + "/hypernyms11ManyPathsOneAncestor.txt";
+        WordNet wn = new WordNet(synsets, hypernyms);
+        Iterable<String> ns = wn.nouns();
+        for (String s: ns) {
+            System.out.printf("%s\n", s);
+        }
+        String w = "yes";
+        System.out.printf("isNoun(%s) = %s\n", w, wn.isNoun(w));
+        w = "a";
+        System.out.printf("isNoun(%s) = %s\n", w, wn.isNoun(w));
+    }
+    // do unit testing of this class
+    public static void main(String[] args) {
+        test();
+    }
 }
