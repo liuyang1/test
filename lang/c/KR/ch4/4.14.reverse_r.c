3f9b225b99c6924f8e48af1e36b3ec7b88b8451b
liuyang1 <liuyang1@mail.ustc.edu.cn>
2016-04-13 16:19:32 +0800

    C: K&R: ch4
    - rpn: 4.3, 4.4, 4.5
    - 4.14 reverse_r

---
+++ lang/c/KR/ch4/4.14.reverse_r.c
@@ -0,0 +1,33 @@
+#include "../kr.h"
+
+// signed integer is two-complement representation.
+// so INT_MIN will overflow.
+
+void reverse_r_i(char *s, int len, int idx) {
+    if (idx < 0) {
+        return;
+    }
+    char t;
+    t = s[idx];
+    s[idx] = s[len - 1 - idx];
+    s[len - 1 - idx] = t;
+    reverse_r_i(s, len, idx - 1);
+}
+
+void reverse_r(char *s) {
+    int len = strlen(s);
+    int half = len / 2 - 1;
+    reverse_r_i(s, len, half);
+}
+
+#define CASE(s) {char *r = strdup(s); reverse_r(r); \
+    char *e = strdup(s); reverse(e); \
+    printf("%s -> %s ?= %s %s\n", \
+           s, r, e, expect(strcmp(r, e) == 0)); }
+
+int main() {
+    CASE("hello");
+    CASE("yeah");
+    CASE("complex one case");
+    return 0;
+}
--- lang/c/KR/ch4/rpn.c
+++ lang/c/KR/ch4/rpn.c
@@ -1,26 +1,27 @@
 #include "../kr.h"
 
-#define errmsg(...) fprintf(stderr, "error: "__VA_ARGS__)
-#define MAXOP       100
+#define debug
+#ifdef debug
+#define log(fmt, ...)   printf("%s:%d " fmt "\n", __FUNCTION__, __LINE__, \
+                               __VA_ARGS__);
+#else
+#define log(...)
+#endif
+#define errmsg(...)     fprintf(stderr, "error: "__VA_ARGS__)
+#define MAXOP           100
 typedef enum {
-    eEOF = EOF,
     eNumber = '0',
     eOperator = 'o',
-    eVarName = 'v',
-    eAdd = '+',
-    eMinus = '-',
-    eMul = '*',
-    eDiv = '/',
-    eMod = '%',
-    eEnd = '\n',
+    // eVarName = 'v',
 } OpType;
 
-#define BUFSIZE 100
+#define BUFSIZE         100
 char buf[BUFSIZE];
 int bufp = 0;
 int getch(void) {
     return (bufp > 0) ? buf[--bufp] : getchar();
 }
+
 void ungetch(int c) {
     if (bufp >= BUFSIZE) {
         errmsg("too many char\n");
@@ -28,33 +29,45 @@ void ungetch(int c) {
     }
     buf[bufp++] = c;
 }
+
+bool oneOf(char c, char s[]) {
+    for (; *s != '\0'; s++) {
+        if (c == *s) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isNumber(char s[]) {
+    int i = 0;
+    if (oneOf(s[i], "+-")) {
+        i++;
+    }
+    if (isdigit(s[i]) || (s[i] == '.' && isdigit(s[i + 1]))) {
+        return true;
+    }
+    return false;
+}
+
 // getop: get next operator or numeric operand
-OpType getop(char *s) {
+int getop(char *s) {
     int i, c;
     while ((s[0] = c = getch()) == ' ' || c == '\t') {
         ;
     }
-    s[1] = '\0';
-    if (!isdigit(c) && c != '.') {
-        return c;
+    if (c == '\n' || c == EOF) {
+        s[1] = '\0';
+        return eOperator;
     }
     i = 0;
-    if (isdigit(c)) {
-        // collect integer part
-        while (isdigit(s[++i] = c = getch())) {
-            ;
-        }
+    while (!(oneOf(s[++i] = c = getch(), " \t\n"))) {
+        ;
     }
-    if (c == '.') {
-        while (isdigit(s[++i] = c = getch())) {
-            ;
-        }
-    }
+    ungetch(c);
     s[i] = '\0';
-    if (c != EOF) {
-        ungetch(c);
-    }
-    return eNumber;
+    log("s=[%s]", s);
+    return isNumber(s) ? eNumber : eOperator;
 }
 
 // maximum depth of val stack
@@ -67,7 +80,7 @@ double val[MAXDEPTH];
 void push(double f) {
     if (sp >= MAXDEPTH) {
         errmsg("stack full, cannot push %g\n", f);
-        return;
+        exit(-1);
     }
     val[sp++] = f;
 }
@@ -81,47 +94,78 @@ double pop(void) {
     return val[--sp];
 }
 
+void clear() {
+    while (sp != 0) {
+        pop();
+    }
+}
+
 int rpn() {
     OpType type;
-    double op2;
+    double op1, op2;
     char s[MAXOP];
 
-    while ((type = getop(s)) != eEOF) {
+    while (true) {
+        type = getop(s);
         switch (type) {
             case eNumber:
                 push(atof(s));
                 break;
-            case '+':
-                push(pop() + pop());
-                break;
-            case '*':
-                push(pop() - pop());
-                break;
-            case '-':
-                op2 = pop();
-                push(pop() - op2);
-                break;
-            case '/':
-                op2 = pop();
-                if (op2 == 0.0) {
-                    errmsg("zero divisor\n");
-                    return -1;
+            case eOperator:
+                if (strcmp(s, "+") == 0) {
+                    push(pop() + pop());
+                } else if (strcmp(s, "*") == 0) {
+                    push(pop() * pop());
+                } else if (strcmp(s, "-") == 0) {
+                    op2 = pop();
+                    push(pop() - op2);
+                } else if (strcmp(s, "/") == 0) {
+                    op2 = pop();
+                    if (op2 == 0.0) {
+                        errmsg("zero divisor\n");
+                        return -1;
+                    }
+                    push(pop() / op2);
+                } else if (strcmp(s, "%") == 0) {
+                    op2 = pop();
+                    if (op2 == 0.0) {
+                        errmsg("zero modulor\n");
+                        return -1;
+                    }
+                    push((int)(pop()) % (int)op2);
+                } else if (strcmp(s, "\n") == 0) {
+                    printf("\t%.8g\n", pop());
+                } else if (strcmp(s, "print") == 0) {
+                    op2 = pop();
+                    printf("top\t%.8g\n", op2);
+                    push(op2);
+                } else if (strcmp(s, "dup") == 0) {
+                    op2 = pop();
+                    push(op2);
+                    push(op2);
+                } else if (strcmp(s, "swap") == 0) {
+                    op1 = pop();
+                    op2 = pop();
+                    push(op1);
+                    push(op2);
+                } else if (strcmp(s, "clear") == 0) {
+                    clear();
+                } else if (strcmp(s, "sin") == 0) {
+                    push(sin(pop()));
+                } else if (strcmp(s, "exp") == 0) {
+                    push(exp(pop()));
+                } else if (strcmp(s, "pow") == 0) {
+                    op2 = pop();
+                    push(pow(pop(), op2));
+                } else if (s[0] == EOF) {
+                    log("%s", "end of file, exit");
+                    return 0;
+                } else {
+                    errmsg("unknown command %x %s\n", s[0], s);
                 }
-                push(pop() / op2);
-                break;
-            case '%':
-                op2 = pop();
-                if (op2 == 0.0) {
-                    errmsg("zero modulor\n");
-                    return -1;
-                }
-                push((int)(pop()) % (int)op2);
-                break;
-            case '\n':
-                printf("\t%.8g\n", pop());
                 break;
             default:
-                errmsg("unknown command %s\n", s);
+                errmsg("unknown type=%x\n", type);
                 break;
         }
     }
