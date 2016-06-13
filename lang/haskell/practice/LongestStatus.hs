f41d20e3fdd8437d251fa86eb95370e807156240
liuyang1 <liuyang1@mail.ustc.edu.cn>
2016-06-13 21:51:13 +0800

    Haskell: LongestStatus: improve perf
    - remove toLower, which is too slow
    - use ByteString instead of String, Text
    - fix arg compatible issue

--- lang/haskell/practice/LongestStatus.hs
+++ lang/haskell/practice/LongestStatus.hs
@@ -1,34 +1,34 @@
+{- status xxx in log
+find all status log, and find which status stay longest, and stay times.
+-}
+
 import System.Environment
 import Data.Ord
 import Data.List
-import Data.Char
-import Data.Text (breakOn, breakOnEnd, pack, unpack)
 import Control.Arrow
 import Control.Monad
-
-{- status xxx in log
--- find all status log, and find which status stay longest, and stay times.
--}
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
 
 -- split log to line
-linesLog = lines . map toLower
+linesLog = C.lines
 
 -- style 0.
 -- allfilt :: [(a-> Bool)] -> (a -> Bool)
 -- allfilt fns = \x -> all (\fn -> fn x) fns
 --
--- isStatus = allfilt [isInfixOf "buffers",  isInfixOf " status "]
+-- isStatus = allfilt [B.isInfixOf (C.pack "buffers"),  B.isInfixOf (C.pack " status ")]
 
 -- style 1.0.
--- (.&&.) f g a = (f a) && (g a)
+(.&&.) f g a = (f a) && (g a)
 
--- style 1.1, point-free with liftM2
-(.&&.) = liftM2 (&&)
+-- style 1.1, point-free with liftM2, a little slow
+-- (.&&.) = liftM2 (&&)
 
-isStatus = isInfixOf "buffers" .&&. isInfixOf " status "
+isStatus = B.isInfixOf (C.pack "BufferS") .&&. B.isInfixOf (C.pack " status ")
 
 -- find status in bufferstatus line log
-probeState = unpack . snd . breakOnEnd (pack " status ") . pack
+probeState = snd . B.breakSubstring (C.pack " status ")
 
 -- max Run Length Encoding
 maxRLE = maximumBy (comparing snd) . map (head &&& length) . group
@@ -36,6 +36,6 @@ maxRLE = maximumBy (comparing snd) . map (head &&& length) . group
 process = maxRLE . map probeState . filter isStatus . linesLog
 
 main = do
-        [f, _] <- getArgs
-        s <- readFile f
+        args  <- getArgs
+        s <- C.readFile $ head args
         print $ process s
