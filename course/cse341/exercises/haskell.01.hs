844c44baacb1c809b08c16e459e495646c232ad1
liuyang1 <liuyang1@mail.ustc.edu.cn>
2016-06-15 22:14:52 +0800

    Haskell: cse341: exercise 1, 2

---
+++ course/cse341/exercises/haskell.01.hs
@@ -0,0 +1,52 @@
+{- 01
+"squid" ++ "clam" -- String
+[True, False, True, True] -- [Bool]
+[True, False, 'a'] -- error
+(True, False, 'a') -- (Bool, Bool, Char)
+-}
+
+cube :: Double -> Double
+cube x = x * x * x
+
+sum :: Double -> Double -> Double -> Double
+sum x y z = x + y + z
+
+quad :: Double -> Double -> Double -> Double -> Double
+quad a b c x = 
+    a * x * x + b * x + c
+
+rev :: [a] -> [a]
+rev [] = []
+rev (x:xs) = rev xs ++ [x]
+
+myMap2 :: (a -> b -> t) -> [a] -> [b] -> [t]
+myMap2 f xs ys = map (uncurry f) $ zip xs ys
+-- myMap2 = zipWith
+
+doubles = 10: map (* 2) doubles
+
+doubles1 = iterate (* 2) 10
+
+dollars = iterate (* 1.05) 100
+
+myConst :: t1 -> t -> t1
+myConst c x = c
+
+-- myConst True :: t -> Bool
+
+append :: [a] -> [a] -> [a]
+append [] ys = ys
+append (x:xs) ys = x: append xs ys
+
+-- append [] :: [a] -> [a]
+-- append [True] :: [Bool] -> [Bool]
+
+myMap :: (t -> a) -> [t] -> [a]
+myMap f [] = []
+myMap f (x: xs) = f x: myMap f xs
+
+-- myMap (myConst True) :: [t] -> [Bool]
+
+-- myConst 5 "octopus" => 5
+-- myMap (myConst "squid") [1 ..] => ["squid", "squid", ..]
+-- myMap sqrt [1, 2, 100] => [1, 1.414, 10]
---
+++ course/cse341/exercises/haskell.02.hs
@@ -0,0 +1,37 @@
+member :: Eq a => a -> [a] -> Bool
+member x [] = False
+member x (y:ys) | x == y = True
+  | otherwise = member x ys
+
+mystery = 1 : map (*2) mystery
+
+ints = scanl1 (+) . zipWith (*) [0..] . concat $ repeat [-1, 1]
+
+data Tree a = EmptyTree
+  | Node a (Tree a) (Tree a)
+  deriving (Show, Read)
+
+inorder :: Tree a -> [a]
+inorder EmptyTree = []
+inorder (Node x l r) = inorder l ++ [x] ++ inorder r
+
+postoder EmptyTree = []
+postoder (Node x l r) = postoder l ++ postoder r ++ [x]
+
+preorder EmptyTree = []
+preorder (Node x l r) = [x] ++ preorder l ++ preorder r
+
+small = (Node 4 EmptyTree (Node 5 EmptyTree EmptyTree))
+big = (Node 10 small (Node 20 EmptyTree small))
+
+data List a = EmptyList
+  | LNode a (List a)
+  deriving (Read, Show)
+
+append EmptyList ys = ys
+append (LNode a b) ys = LNode a (append b ys)
+
+lst = LNode 2 (LNode 1 EmptyList)
+
+mymap _ EmptyList = EmptyList
+mymap f (LNode a b) = LNode (f a) (mymap f b)
