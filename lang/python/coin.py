8fb07dcae1598e259489e080d97670824f109976
liuyang1 <liuyang1@mail.ustc.edu.cn>
2016-10-11 21:42:19 +0800

    Python: coin: add theory result

--- lang/python/coin.py
+++ lang/python/coin.py
@@ -5,15 +5,17 @@ Assume we could get any kind of coin with same probablity,
        we want to collect all kinds of quaters.
 How many quaters need to get in average?
 
-246.5
+247.073
 """
 import random
 
 N = 54
 
+
 def getCoin():
     """ [0, N -1] """
     return random.randint(0, N - 1)
+
 
 def getFullSet():
     cnt = 0
@@ -21,9 +23,9 @@ def getFullSet():
     while True:
         st.add(getCoin())
         cnt += 1
-        if len(st) == N: # get all kinds of coins
+        if len(st) == N:  # get all kinds of coins
             return cnt
-        
+
 
 def avgNum():
     times = 10000
@@ -31,3 +33,11 @@ def avgNum():
     return sum(lst) / (len(lst) + 0.0)
 
 print avgNum()
+
+def inTheory():
+    s = 0
+    for i in range(1, N + 1):
+        s += N / (i + 0.0)
+    return s
+
+print inTheory()
