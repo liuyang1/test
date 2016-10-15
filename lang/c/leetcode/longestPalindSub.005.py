fc7bdd2d87866829effab32fc85e5b194acb09c7
liuyang1 <liuyang1@mail.ustc.edu.cn>
2016-10-15 22:34:35 +0800

    update longestPalindSub.005.py

--- lang/c/leetcode/longestPalindSub.005.py
+++ lang/c/leetcode/longestPalindSub.005.py
@@ -1,59 +1,61 @@
-def callIter(s, init, iterFn, retFn):
-    lst, r = init(s)
-    if len(lst) == 0:
-        return ""
-    while True:
-        one = lst[0]
-        newlst = iterFn(s, lst, r)
-        if len(newlst) == 0:
-            r -= 1
-            return retFn(s, one, r)
-        lst = newlst
-        r += 1
+#! /usr/bin/env python2
+
+"""
+find longest palindrom substring
+https://leetcode.com/problems/longest-palindromic-substring/
+
+ref:
+- http://taop.marchtea.com/01.05.html
+- https://en.wikipedia.org/wiki/Longest_palindromic_substring
+
+"""
+def bestSeq(n):
+    # use bestSeq will find most potential longest substring,
+    # but will return wrong answer when want to find FIRST substring
+    l = range(n)
+    o = [i for i in l if i % 2 == 1]
+    e = [i for i in l if i % 2 == 0]
+    r = [v for p in zip(o[::-1], e[::-1]) for v in p]
+    if len(o) != len(e):
+        r.append(e[0])
+    return r
 
 
-def searchOdd(s):
-    def init(s):
-        return [i for i in range(0, len(s))], 1
-
-    def iterOdd(s, lst, r):
-        def isSameRadius(i):
-            return i - r >= 0 and i + r < len(s) and s[i - r] == s[i + r]
-        return [i for i in lst if isSameRadius(i)]
-
-    def retOdd(s, m, r):
-        return s[m - r: m + r + 1]
-    return callIter(s, init, iterOdd, retOdd)
-
-
-def searchEven(s):
-    def init(s):
-        return [i for i in range(0, len(s) - 1)], 0
-
-    def iterEven(s, lst, r):
-        def isSameRadius(i):
-            return i - r >= 0 and i + r + 1 < len(s) and s[i - r] == s[i + r + 1]
-        return [i for i in lst if isSameRadius(i)]
-
-    def retEven(s, m, r):
-        return s[m - r: m + r + 2]
-    return callIter(s, init, iterEven, retEven)
-
-
-def searchLongstPalind(s):
-    odd = searchOdd(s)
-    even = searchEven(s)
-    return odd if len(odd) > len(even) else even
+def longestPalindrome_1(s):
+    """
+    O(n ^ 2) solution
+    """
+    if len(s) == 0:
+        return s
+    mp, ml = 0, 0
+    for i in range(0, len(s)):
+        l = -1
+        for r in range(0, len(s)):
+            if not (i - r >= 0 and i + r < len(s) and s[i - r] == s[i + r]):
+                break
+            p, l = i - r, 2 * r + 1
+        if l > ml:
+            mp, ml = p, l
+        for r in range(0, len(s)):
+            if not (i - r >= 0 and i + r + 1 < len(s) and s[i - r] == s[i + r + 1]):
+                break
+            p, l = i - r, 2 * r + 2
+        if l > ml:
+            mp, ml = p, l
+        if ml == len(s):
+            break
+    return s[mp: mp + ml]
 
 
 class Solution(object):
 
     def longestPalindrome(self, s):
-        return searchLongstPalind(s)
+        return longestPalindrome_1(s)
 
     def check(self, s, e):
         r = self.longestPalindrome(s)
         if r == e:
+            # print "longestPalindrome('%s')='%s' == '%s' SUCC" % (s, r, e)
             return True
         print "longestPalindrome('%s')='%s' != '%s' FAIL" % (s, r, e)
         exit(-1)
@@ -73,6 +75,8 @@ def testPositive():
     sln.check(s + "aaaa", s)
     sln.check("aaaa" + s, s)
     sln.check("abac", "aba")
+    # must return first substring
+    sln.check("abcda", "a")
 
 
 def testPerf():
