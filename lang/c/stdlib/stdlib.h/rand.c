886596ddc5ac4c5600e6e106f4361671384afbc0
liuyang1 <liuyang1@mail.ustc.edu.cn>
2017-07-13 09:44:32 +0800

    C: stdlib: strtoul error handling

--- lang/c/stdlib/stdlib.h/rand.c
+++ lang/c/stdlib/stdlib.h/rand.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,5 +6,8 @@ int main() {
     // In the GNU C Library, it is 2147483647, which is the largest signed integer representable in
     // 32 bits. In other libraries, it may be as low as 32767.
     printf("%s: %d\n", "RAND_MAX", RAND_MAX);
+    printf("RAND_MAX=%d ?= INT_MAX=%d %s\n",
+           RAND_MAX, INT_MAX,
+           RAND_MAX == INT_MAX ? "true" : "false");
     return 0;
 }
--- lang/c/stdlib/stdlib.h/strtoul.c
+++ lang/c/stdlib/stdlib.h/strtoul.c
@@ -51,10 +51,48 @@ int testloop(void (*fn)(char*))
         fn(s[i]);
     }
 }
+
+/** NOTES
+ * - when strtoul success, it return [0, ULONG_MAX];
+ * - when strtoul fail, it return 0 or ULONG_MAX, and setting errno;
+ *   so if strtoul return 0 or ULONG_MAX, we must check errno, and have to setting it to zero before
+ *   calling;
+ *   also, need check endptr return value.
+ */
+/** COMMENT
+ * This is a bad design style, It's so hard for user to write correctly program.
+ */
+#include <errno.h>
+#include <string.h>
+void testFail(char *s) {
+    errno = 0;
+    char *endptr = NULL;
+    unsigned long i = strtoul(s, &endptr, 0);
+
+    printf("%lu \t\t errno:%d(%s)\n", i, errno, strerror(errno));
+    if (errno != 0) {
+        printf("detec error\n");
+    }
+
+    printf("nptr=%p endptr=%p diff=%ld\n", s, endptr, endptr - s);
+    printf(">>> ");
+    if (*endptr == '\0') {
+        printf("successfully parse it to end of string\n");
+    } else if(endptr == s) {
+        printf("nothing to parse\n");
+    } else {
+        printf("cannot parse when to endptr\n");
+    }
+}
+
 int main(int argc, char *argv[])
 {
     testloop(test);
     testloop(test2);
     testloop(test3);
+
+    testFail("123");
+    testFail("abc");
+    testFail("12345678900000000000000000000");
     return 0;
 }
