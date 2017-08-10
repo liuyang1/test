f6299c26f053272ad50f6014068a0560a9d4f664
liuyang1 <liuyang1@mail.ustc.edu.cn>
2017-08-10 16:22:36 +0800

    racket: List: fold, list transform

--- lang/lisp/racket/YetAnotherWheel/lst.rkt
+++ lang/lisp/racket/YetAnotherWheel/lst.rkt
@@ -101,4 +101,107 @@
 ;;; TOOD: after concat
 ; (define (intercalate xs xss))
 
-; (define (transpose xss))
+;;; Reducing Lists (folds)
+
+;; foldl :: Foldable t => (b -> a -> b) -> b -> t a -> b
+(define (foldr op unit xs)
+  (if (null? xs)
+    unit
+    (op (car xs)
+        (foldr op unit (cdr xs)))))
+
+(equal? (foldr cons '() (range 3))
+        '(0 1 2))
+
+(define (foldl op unit xs)
+  (if (null? xs)
+    unit
+    (foldl op (op unit (car xs)) (cdr xs))))
+
+(equal? (foldl cons '() (range 3))
+        '(((() . 0) . 1) . 2))
+
+(define (foldr1 op xs)
+  (if (null? (cdr xs))
+    (car xs)
+    (op (car xs)
+        (foldr1 op (cdr xs)))))
+
+(equal? (foldr1 / (range 1 5))
+        (/ 1 (/ 2 (/ 3 4))))
+
+;; fold1 :: Foldable t => (a -> a -> a) -> t a -> a
+(define (foldl1 op xs)
+  (if (null? (cdr xs))
+    (car xs)
+    (foldl op (car xs) (cdr xs))))
+
+(equal? (foldl1 / (range 1 5))
+        (/ (/ (/ 1 2) 3) 4))
+
+(define (concat xss)
+  (foldr ++ '() xss))
+
+(define (transpose xss)
+  (let ((new (filter (lambda (xs) (not (null? xs))) xss)))
+   (if (null? new)
+     '()
+     (cons (map car new)
+           (transpose (map cdr new))))))
+
+(and (equal? (transpose '((10 11) (20) () (30 31 32)))
+             '((10 20 30) (11 31) (32)))
+     (equal? (transpose '((10 11 12) (20 21 22) (30 31 32)))
+             '((10 20 30) (11 21 31) (12 22 32))))
+
+;; subsequences:: [a] -> [[a]]
+(define (subsequences xs)
+  (if (null? xs)
+    '(())
+    (let ((x (car xs))
+          (ys (subsequences (cdr xs))))
+      (++ (map (lambda (a) (cons x a)) ys)
+          ys))))
+
+(and (equal? (subsequences '())
+             '(()))
+     (equal? (subsequences '(1))
+             '((1) ()))
+     (equal? (subsequences '(1 2))
+             '((1 2) (1) (2) ()))
+     (equal? (subsequences '(1 2 3))
+             '((1 2 3) (1 2) (1 3) (1) (2 3) (2) (3) ())))
+
+(define (insert x idx xs)
+  (if (= idx 0)
+    (cons x xs)
+    (cons (car xs) (insert x (- idx 1) (cdr xs)))))
+
+;; insert-every:: a -> [a] -> [[a]]
+(define (insert-every x xs)
+  (map (lambda (idx) (insert x idx xs))
+       (range 0 (+ 1 (length xs)))))
+
+(equal? (insert-every -1 (range 0 3))
+        '((-1 0 1 2) (0 -1 1 2) (0 1 -1 2) (0 1 2 -1)))
+
+;; permutations:: [a] -> [[a]]
+(define (permutations xs)
+  (if (or (null? xs) (null? (cdr xs)))
+    (list xs)
+    (let ((x (car xs))
+          (ys (permutations (cdr xs))))
+      (concat
+        (map (lambda (y) (insert-every x y))
+             ys)))))
+
+(and (equal? (permutations '())
+             '(()))
+     (equal? (permutations '(1))
+             '((1)))
+     (equal? (permutations '(1 2))
+             '((1 2) (2 1)))
+     (equal? (permutations '(1 2 3))
+             '((1 2 3) (2 1 3) (2 3 1) (1 3 2) (3 1 2) (3 2 1))))
+
+;;; Special folds
