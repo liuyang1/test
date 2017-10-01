fb850277faa498314358d3e0ae1bfd2a9aa24570
liuyang1 <liuyang1@mail.ustc.edu.cn>
2017-10-02 02:03:05 +0800

    Haskell: review Record

---
+++ lang/haskell/basic/Record.hs
@@ -0,0 +1,7 @@
+findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey key = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing
+
+boolToNum True = 1
+boolToNum False = 0
+
+findKeyTimes key = foldr (\(k, v) acc -> acc + (boolToNum (key == k))) 0
--- lang/haskell/findkey.hs
+++
@@ -1,2 +0,0 @@
-findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
-findKey key = foldr (\(k,v) aac -> if key == k then Just v else aac) Nothing
