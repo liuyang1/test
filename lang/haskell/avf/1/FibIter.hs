1722441b378828174be905c7beb3f5e74283c323
liuyang1 <liuyang1@mail.ustc.edu.cn>
2017-10-02 21:59:36 +0800

    Haskell: review AVF

--- lang/haskell/avf/1/FibIter.hs
+++ lang/haskell/avf/1/FibIter.hs
@@ -4,3 +4,13 @@ ffib (a, b) = (b, a + b)
 
 fib = [i | (_, i) <- xs]
   where xs = take 100 $ iterate ffib (1, 1)
+
+-- this is more compact and simple
+
+one = (1, 1)
+fibnext (a, b) = (b, a + b)
+
+fib' = map snd $ iterate fibnext one
+
+main = do
+        print $ take 10 fib'
--- lang/haskell/gist/PolyVariadic.hs
+++ lang/haskell/gist/PolyVariadic.hs
@@ -7,9 +7,9 @@ instance SumRes Integer where
     sumOf = id
 
 -- | sumOf could add variadic arguments
--- >>> sumOf 1 2 3 :: Integer
--- 6
--- >>> sumOf 4 5 :: Integer
--- 9
 instance (Integral a, SumRes r) => SumRes (a -> r) where
     sumOf x = sumOf . (x +) . toInteger
+
+main = do
+        print $ 6 == (sumOf 1 2 3 :: Integer)
+        print $ 9 == (sumOf 4 5 :: Integer)
