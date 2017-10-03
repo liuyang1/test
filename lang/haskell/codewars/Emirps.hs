0e64802419db2a93981db0168383fdde998fb488
liuyang1 <liuyang1@mail.ustc.edu.cn>
2017-10-03 14:17:51 +0800

    Haskell: codewars: add Emirps

---
+++ lang/haskell/codewars/Emirps.hs
@@ -0,0 +1,24 @@
+module Emirps where
+
+import Prime
+
+-- Emirps is a prime sequence, whose reversed num is a different prime.
+-- ref: https://oeis.org/A006567
+
+rev ::  Integer -> Integer
+rev = read . reverse . show
+
+inOrdered x [] = False
+inOrdered x (y:ys) = case (compare x y) of
+                       LT -> False
+                       EQ -> True
+                       GT -> inOrdered x ys
+
+emirps = filter (\x -> let r = rev x in r /= x && r `inOrdered` primes) primes
+
+stat [] = [0, 0, 0]
+stat xs = map ($ xs) [toInteger . length, last, sum]
+
+findEmirps n = stat $ takeWhile (<= n) emirps
+
+main = print $ findEmirps 7523
