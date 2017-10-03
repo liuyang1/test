03903e133efadd37a89f6b67d7a84ada478abc4c
liuyang1 <liuyang1@mail.ustc.edu.cn>
2017-10-03 17:16:58 +0800

    Haskell: improve Factor

--- lang/haskell/basic/math/Factor.hs
+++ lang/haskell/basic/math/Factor.hs
@@ -11,5 +11,14 @@ f n pp@(p:ps)
 
 factor n = f n primes
 
+-- better solution
+-- - don't depend on prime sequence
+-- - save memory
+factor' n = hlp n 2
+    where hlp n i
+              | i * i > n = [n]
+              | n `mod` i == 0 = i: (hlp (n `quot` i) i)
+              | otherwise = hlp n (i + 1)
+
 factorg n = map (head &&& length) $ group fs
-        where fs = factor n
+        where fs = factor' n
