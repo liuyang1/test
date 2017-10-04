95dfa8766625997be05a801665eb567c794a037f
liuyang1 <liuyang1@mail.ustc.edu.cn>
2017-10-04 17:23:15 +0800

    TFH: ch01: improve G

--- lang/haskell/ThinkingFunctionally/ch01/G.hs
+++ lang/haskell/ThinkingFunctionally/ch01/G.hs
@@ -1,15 +1,15 @@
 import Data.Char (toUpper)
-import Data.List (intersperse)
+import Data.List (intercalate)
 
 -- 建议始终加上函数的类型，便于阅读。毕竟程序是写给程序员的，而不是机器的。
 song :: Int -> String
-song n = concat $ intersperse "\n" $ map verse [1..n]
+song n = intercalate "\n\n" $ map verse [1..n]
 
 capital :: String -> String
 capital (x:xs) = (toUpper x): xs
 
 verse :: Int -> String
-verse n = concatMap ((++ "\n") . capital . ($ n)) [line1, line2, line3, line4]
+verse n = intercalate "\n" $ map (capital . ($ n)) [line1, line2, line3, line4]
 
 showNum :: Int -> String
 showNum n = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"] !! n
@@ -19,16 +19,15 @@ plural n = showNum n ++ man n
     where man 1 = " man"
           man _ = " men"
 
-line1 :: Int -> String
+-- 函数标记可以写在一行里面
+line1, line2, line3, line4 :: Int -> String
+
 line1 n = (plural n) ++ " went to mow"
 
-line2 :: Int -> String
 line2 _ = "went to mow a meadow"
 
-line3 :: Int -> String
-line3 n = (concat $ intersperse ", " $ map plural [n, n - 1.. 1]) ++ " and his dog"
+line3 n = (intercalate ", " $ map plural [n, n - 1.. 1]) ++ " and his dog"
 
-line4 :: Int -> String
 line4 = line2
 
 main = putStrLn $ song 5
