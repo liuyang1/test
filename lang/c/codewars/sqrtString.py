b26a59c3bc6d152c52d08a33e4c6167e6987646f
liuyang1 <liuyang1@mail.ustc.edu.cn>
2017-11-05 20:47:47 +0800

    C: codewars: sqrtString: add comment for python demo

--- lang/c/codewars/sqrtString.py
+++ lang/c/codewars/sqrtString.py
@@ -1,27 +1,44 @@
+#!/usr/bin/env python2
+#-*- encoding:utf8
+"""
+启发自Thinking Functionaly with Haskell一书，P36的floor函数实现方法
+"""
+
+
 def sqrt(n):
+    def invFunc(i):
+        return i * i
+
     def bound():
         i = 1
-        while i * i < n:
+        while invFunc(i) <= n: # must add equal
             i *= 2
         return i
+
     def shrink(a, b):
         m = (a + b) / 2
-        m2 = m * m
+        m2 = invFunc(m)
         if m2 == n:
             return m, m
-        elif m * m > n:
+        elif m2 > n:
             return a, m
         else:
             return m, b
+
+    def unit(b, e):
+        return e - b <= 1
+
     b, e = 0, bound()
-    step = 1
     while True:
-        step += 1
         b, e = shrink(b, e)
-        if e - b <= 1:
-            print "step: ", step
-            return b
+        if unit(b, e):
+            break
+    return b
 
 
-print sqrt(101)
+for i in range(200):
+    sqr = sqrt(i)
+    if not sqr * sqr <= i < (sqr + 1) * (sqr + 1):
+        print "find except on %d, %d" % (i, sqr)
+        break
 print sqrt(23232328323215435345345345343458098856756556809400840980980980980809092343243243243243098799634)
