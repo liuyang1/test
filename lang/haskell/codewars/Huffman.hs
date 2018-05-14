13f3e440addbdc130769fe6810870a5589b22a88
liuyang1 <liuyang1@mail.ustc.edu.cn>
2018-05-14 09:26:27 +0800

    Haskell: codewars: Huffman: improve error handling

--- lang/haskell/codewars/Huffman.hs
+++ lang/haskell/codewars/Huffman.hs
@@ -29,25 +29,26 @@ getFreq (Node a _ _) = a
 -- must compare with freq instead of raw inheriting Order
 cmpTree = compare `on` getFreq
 
-cmbTree a b = Node (getFreq a + getFreq b) a b
-
-buildHuff [] = error "Huffman: empty list"
-buildHuff [x] = x
-buildHuff (x:y:xs) = buildHuff (insertBy cmpTree (cmbTree x y) xs)
+buildHuff [] = Nothing
+buildHuff [(Leaf _ _)] = Nothing
+buildHuff [r] = Just r
+buildHuff (x:y:xs) = buildHuff $ insertBy cmpTree (cmbTree x y) xs
+    where cmbTree a b = Node (getFreq a + getFreq b) a b
 
 freq2Tree = buildHuff . sortBy cmpTree . map (uncurry Leaf . swap)
 
-insertBit b (a, bs) = (a, b:bs)
-
 tree2Dct (Leaf _ v) = [(v, [])]
-tree2Dct (Node _ left right) = (map (insertBit Z) $ tree2Dct left) ++
-    (map (insertBit O) $ tree2Dct right)
+tree2Dct (Node _ left right) = go Z left ++ go O right
+    where go b t = map (insertBit b) $ tree2Dct t
+          insertBit b (a, bs) = (a, b:bs)
 
-freq2Dct :: Ord k => [(k, Int)] -> Map.Map k [Bit]
-freq2Dct = Map.fromList . tree2Dct . freq2Tree
+freq2Dct :: Ord k => [(k, Int)] -> Maybe (Map.Map k [Bit])
+freq2Dct = fmap Map.fromList . fmap tree2Dct . freq2Tree
 
 encode :: Ord a => [(a, Int)] -> [a] -> Maybe [Bit]
-encode freq = fmap concat . mapM (flip Map.lookup d)
+encode freq bs
+    | d == Nothing = Nothing
+    | otherwise = fmap concat $ mapM (flip Map.lookup (fromJust d)) bs
     where d = freq2Dct freq
 
 decodeTree r (Node _ left _) (Z:bs) = decodeTree r left bs
@@ -56,9 +57,13 @@ decodeTree r (Leaf _ v) [] = Just [v]
 decodeTree r (Leaf _ v) bs = fmap (v:) $ decodeTree r r bs
 decodeTree r _ [] = Nothing
 
-decode :: [(a, Int)] -> [Bit] -> Maybe [a]
-decode freq bs = decodeTree t t bs
+decode :: (Eq a) => [(a, Int)] -> [Bit] -> Maybe [a]
+decode freq bs
+  | t == Nothing = Nothing
+  | bs == [] = Just []
+  | otherwise = decodeTree r r bs
     where t = freq2Tree freq
+          r = fromJust t
 
 norm xs = map (flip (/) s) xs
     where s = sum xs
@@ -80,7 +85,9 @@ cs = "abc"
 bs = encode (frequencies str) cs
 cs1 = "abcd"
 bs1 = encode (frequencies str) cs1
+bs2 = encode ([]) cs
 cs_ = decode (frequencies str) $ fromJust bs
 cs2 = decode (frequencies str) [Z]
+cs3 = decode (frequencies str) []
 
 verify freq cs = ((==) cs) $ fromJust $ decode freq $ fromJust $ encode freq cs
