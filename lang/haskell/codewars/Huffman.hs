2986823aaea5fdd85a25526a84699c0e38775c39
liuyang1 <liuyang1@mail.ustc.edu.cn>
2018-05-14 13:59:28 +0800

    Haskell: codewars: Huffman: clean hlint warning

--- lang/haskell/codewars/Huffman.hs
+++ lang/haskell/codewars/Huffman.hs
@@ -10,8 +10,9 @@ import Data.Function (on)
 import Data.List (sort, group, insertBy, sortBy, nub)
 import Data.Tuple (swap)
 import Control.Arrow ((&&&))
-import qualified Data.Map as Map (fromList, Map, (!), lookup)
+import qualified Data.Map as Map (fromList, Map, lookup)
 import Data.Maybe (fromJust)
+import Control.Applicative ((<$>))
 
 data Bit = Z | O deriving (Show, Eq)
 
@@ -30,7 +31,7 @@ getFreq (Node a _ _) = a
 cmpTree = compare `on` getFreq
 
 buildHuff [] = Nothing
-buildHuff [(Leaf _ _)] = Nothing
+buildHuff [Leaf _ _] = Nothing
 buildHuff [r] = Just r
 buildHuff (x:y:xs) = buildHuff $ insertBy cmpTree (cmbTree x y) xs
     where cmbTree a b = Node (getFreq a + getFreq b) a b
@@ -43,41 +44,38 @@ tree2Dct (Node _ left right) = go Z left ++ go O right
           insertBit b (a, bs) = (a, b:bs)
 
 freq2Dct :: Ord k => [(k, Int)] -> Maybe (Map.Map k [Bit])
-freq2Dct = fmap Map.fromList . fmap tree2Dct . freq2Tree
+freq2Dct = fmap (Map.fromList . tree2Dct) . freq2Tree
 
 encode :: Ord a => [(a, Int)] -> [a] -> Maybe [Bit]
-encode freq bs
-    | d == Nothing = Nothing
-    | otherwise = fmap concat $ mapM (flip Map.lookup (fromJust d)) bs
-    where d = freq2Dct freq
+encode freq bs = do
+    d <- freq2Dct freq
+    concat <$> mapM (`Map.lookup` d) bs
 
 decodeTree r (Node _ left _) (Z:bs) = decodeTree r left bs
 decodeTree r (Node _ _ right) (O:bs) = decodeTree r right bs
 decodeTree r (Leaf _ v) [] = Just [v]
-decodeTree r (Leaf _ v) bs = fmap (v:) $ decodeTree r r bs
+decodeTree r (Leaf _ v) bs = (v:) <$> decodeTree r r bs
 decodeTree r _ [] = Nothing
 
 decode :: (Eq a) => [(a, Int)] -> [Bit] -> Maybe [a]
-decode freq bs
-  | t == Nothing = Nothing
-  | bs == [] = Just []
-  | otherwise = decodeTree r r bs
-    where t = freq2Tree freq
-          r = fromJust t
+decode freq bs = do
+    r <- freq2Tree freq
+    if null bs then return []
+               else decodeTree r r bs
 
-norm xs = map (flip (/) s) xs
+norm xs = map (/ s) xs
     where s = sum xs
 
-h x = -1 * (logBase 2 x)
+h x = -1 * logBase 2 x
 
 entropy xs = sum $ zipWith (*) ys $ map h ys
     where ys = norm xs
 
-upperIdx2 n = ceiling $ logBase 2 $ until ((<=) (fromIntegral n)) ((*) 2) 1
+upperIdx2 n = ceiling $ logBase 2 $ until (fromIntegral n <=) (2 *) 1
 
-compressRatio freq cs = ((/) `on` (toRational))
+compressRatio freq cs = ((/) `on` toRational)
     (length $ fromJust $ encode freq cs)
-    ((length cs) * (upperIdx2 $ length $ nub cs))
+    (length cs * upperIdx2 (length $ nub cs))
 
 str = "aaaabcc"
 
@@ -85,9 +83,9 @@ cs = "abc"
 bs = encode (frequencies str) cs
 cs1 = "abcd"
 bs1 = encode (frequencies str) cs1
-bs2 = encode ([]) cs
+bs2 = encode [] cs
 cs_ = decode (frequencies str) $ fromJust bs
 cs2 = decode (frequencies str) [Z]
 cs3 = decode (frequencies str) []
 
-verify freq cs = ((==) cs) $ fromJust $ decode freq $ fromJust $ encode freq cs
+verify freq cs = (cs ==) $ fromJust $ decode freq $ fromJust $ encode freq cs
