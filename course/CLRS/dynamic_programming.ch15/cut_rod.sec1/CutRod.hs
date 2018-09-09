389a911476acf161b7aae11d684dc5436d03216e
liuyang1 <liuyang1@mail.ustc.edu.cn>
2018-09-09 19:05:18 +0800

    CLRS: ch15: sec1: finish exercise

--- course/CLRS/dynamic_programming.ch15/cut_rod.sec1/CutRod.hs
+++ course/CLRS/dynamic_programming.ch15/cut_rod.sec1/CutRod.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import System.Environment
+import Data.List (maximumBy)
+import Data.Function (on)
 
 prices = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
 
@@ -22,6 +24,33 @@ cutRod2 ps n = go [] 0
     where l = length ps
           go xs i = if i == n then xs else go (x: xs) (i + 1)
               where x = maximum $ [ps !! i | i < l] ++ zipWith (+) xs ps
+
+-- 15.1-3 consider fixed cost on cuting every time
+c = 1
+
+cutRod3 ps n = go [] 0
+    where l = length ps
+          go xs i = if i == n then xs else go (x: xs) (i + 1)
+              where x = maximum $ [ps !! i | i < l] ++ zipWith (+) ps (map (c -) xs)
+
+-- 15.1.-4 return solution
+-- [(value, [slices]), ..]
+
+data Sln = Sln Int [Int] deriving (Show, Eq)
+
+instance Ord Sln where
+    compare (Sln v0 _) (Sln v1 _) = compare v0 v1
+
+add :: Sln -> Sln -> Sln
+add (Sln a x) (Sln b y) = Sln (a + b) (x++y)
+
+up a b = Sln a [b]
+
+cutRod4 ps n = go [] 0
+    where l = length ps
+          ps_ = zipWith up ps [1..]
+          go xs i = if i == n then xs else go (x: xs) (i + 1)
+              where x = maximum $ [ps_ !! i | i < l] ++ zipWith add ps_ xs
 
 main = do args <- getArgs
           print $ cutRod1 prices $ read $ head args
