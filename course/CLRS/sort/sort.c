4c670f044c5c5c75f212760d20f7121ef256ec99
liuyang1 <liuyang1@mail.ustc.edu.cn>
2018-09-15 17:34:30 +0800

    sort: bubble, select, merge sort

--- course/CLRS/sort/sort.c
+++ course/CLRS/sort/sort.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <assert.h>
 
 typedef int (*CmpFun)(const void *, const void *);
+typedef void (*SortFun)(void *base, size_t nmemb, size_t size, CmpFun cmp);
 
 int cmp_int(const void *a, const void *b) {
     const int *pa = a;
@@ -19,9 +21,94 @@ void show_int_arr(unsigned int *base, size_t nmemb) {
     printf("\n");
 }
 
+static void assign(void *a, void *b, size_t size) {
+    assert(a != NULL && b != NULL && size != 0 && a != b);
+    memcpy(a, b, size);
+}
+
+static void swap(void *a, void *b,  size_t size, void *t) {
+    assign(t, a, size);
+    assign(a, b, size);
+    assign(b, t, size);
+}
+
+/** always pick the minimum one, then swap it to head
+ */
+void select_sort(void *base, size_t nmemb, size_t size, CmpFun cmp) {
+    size_t i, j, midx;
+    void *t = malloc(size);
+    int cnt = 0;
+    for (i = 0; i != nmemb - 1; i++) {
+        for (midx = i, j = i + 1; j != nmemb; j++) {
+            if (cmp(base + midx * size, base + j * size) > 0) {
+                midx = j;
+            }
+            cnt++;
+        }
+        if ( i != midx ) {
+            swap(base + i * size, base + midx * size, size, t);
+        }
+    }
+    printf("%d\n", cnt);
+    free(t);
+}
+
+/** check pair every time, reorder it
+ * This will floating one biggest bubble to END every time
+ * so we could check the END position to saving useless loop
+ */
+void bubble_sort(void *base, size_t nmemb, size_t size, CmpFun cmp) {
+    size_t i, j, tail, ntail;
+    void *t = malloc(size);
+    for (i = 0, tail = nmemb - 1; i != nmemb; i++) {
+        for (j = 0; j != tail; j++) {
+            if (cmp(base + j * size, base + (j + 1) * size) > 0) {
+                swap(base + j * size, base + (j + 1) * size, size, t);
+                ntail = j;
+            }
+        }
+        if (tail == ntail) {
+            break;
+        }
+        tail = ntail;
+    }
+    free(t);
+}
+
+/** Merge sort
+ *  when merge to two sub sequences, we need extra storage.
+ */
+void merge_sort(void *base, size_t nmemb, size_t size, CmpFun cmp) {
+    if (nmemb <= 1) {
+        return;
+    }
+    size_t mid = nmemb / 2;
+    merge_sort(base, mid, size, cmp);
+    merge_sort(base + mid * size, nmemb - mid, size, cmp);
+    void *t = malloc(size * nmemb);
+    size_t i, j, k, idx;
+    for (k = i = 0, j = mid; i != mid && j != nmemb; k++) {
+        if (cmp(base + i * size, base + j * size) <= 0) {
+            idx = i++;
+        } else {
+            idx = j++;
+        }
+        assign(t + k * size, base + idx * size, size);
+    }
+    for (; i != mid; i++, k++) {
+        assign(t + k * size, base + i * size, size);
+    }
+    for (; j != nmemb; j++, k++) {
+        assign(t + k * size, base + j * size, size);
+    }
+    assert(k == nmemb);
+    memcpy(base, t, size * nmemb);
+    free(t);
+}
+
 /** Binary Tree with quick sort
  * ref:
- *https://tekmarathon.com/2013/09/20/analogy-between-binary-search-tree-and-quicksort-algorithm/
+ * https://tekmarathon.com/2013/09/20/analogy-between-binary-search-tree-and-quicksort-algorithm/
  */
 
 typedef struct Node {
@@ -48,10 +135,6 @@ void show_int_bin_tree(Node *n) {
     show_int_bin_tree_in(n, 0);
 }
 
-static void assign(void *a, void *b, size_t size) {
-    memcpy(a, b, size);
-}
-
 void free_bin_tree(Node *n) {
     if (n == NULL) {
         return;
@@ -62,7 +145,8 @@ void free_bin_tree(Node *n) {
     free(n);
 }
 
-static Node *qsort_bintree_in(void *base, size_t nmemb, size_t size, CmpFun cmp) {
+static Node *qsort_bintree_in(void *base, size_t nmemb, size_t size,
+                              CmpFun cmp) {
     char *p = base;
     if (nmemb == 0) {
         return NULL;
@@ -136,6 +220,7 @@ void radix_sort(ValT *p, size_t num) {
             }
         }
         assert(m + n == num);
+        /** End when sorted, skip useless checking */
         if (m == 0 || n == 0) {
             break;
         }
@@ -155,7 +240,10 @@ int main() {
     size_t n = sizeof(a) / sizeof(a[0]);
     show_int_arr(a, n);
     // qsort_bintree(a, n, sizeof(a[0]), cmp_int);
-    radix_sort(a, n);
+    // radix_sort(a, n);
+    // select_sort(a, n, sizeof(a[0]), cmp_int);
+    // bubble_sort(a, n, sizeof(a[0]), cmp_int);
+    merge_sort(a, n, sizeof(a[0]), cmp_int);
     show_int_arr(a, n);
     return 0;
 }
