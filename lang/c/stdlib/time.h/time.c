00dcc6ffadc6f3a7f0dd72029e32059866ff3153
liuyang1 <liuyang1@mail.ustc.edu.cn>
2018-10-22 14:35:48 +0800

    C: time.h: update test

--- lang/c/stdlib/time.h/time.c
+++ lang/c/stdlib/time.h/time.c
@@ -2,15 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
-	time_t now;
-	struct tm *timenow;
-	time(&now);
-	printf("%d\n",now);
-	timenow=localtime(&now);
-	printf("local time is %d\t%s\n",now,asctime(timenow));
+int main() {
+    time_t uninit;
+    time_t now;
+    struct tm *timenow;
+
+    time(&now);
+    printf("%d\n", now);
+
+    timenow = localtime(&now);
+    printf("local time is %d\t%s\n", now, asctime(timenow));
+    // ctime = localtime(asctime(struct tim*));
+    printf("ctime %s\n", ctime(&now));
+
+    printf("ctime uninit: %s\n", ctime(&uninit));
+
     char buf[40];
-    strftime(buf, 40, "%H%M%S.%Y%m%d", timenow);
+    strftime(buf, 40, "%H:%M:%S %Y-%m-%d", timenow);
     printf("strftime: %s\n", buf);
-	return 0;
+    return 0;
 }
