b6617f7e1c600882e7488a47ea331a51ffe54d47
liuyang1 <liuyang1@mail.ustc.edu.cn>
2019-02-20 15:48:22 +0800

    Haskell: review MultilinearPolynomials.hs

--- lang/haskell/codewars/MultilinearPolynomials.hs
+++ lang/haskell/codewars/MultilinearPolynomials.hs
@@ -10,56 +10,35 @@ Stability   : experimental
 module MultilinearPolynomials where
 -- module Codewars.Kirilloid.MultilinearPolynomials where
 
-import Data.List
-import Data.Char
-import Data.Function
-import Data.Monoid
+import           Data.Char
+import           Data.Function
+import           Data.List
+import           Data.Monoid
 
 -- | split string by this function. It group multi-linear var and coeff
-fn x y = isDigit y || (isAlpha x && isAlpha y)
+-- >>> groupBy coeffOrVar "a-245cb+zyx"
+-- ["a", "-245", "cb", "+", "zyx"]
+coeffOrVar x y = isDigit y || (isAlpha x && isAlpha y)
+
+isVar x = isAlpha $ head x
+
 -- | add extra plug sign + if first item have +1 coeff, which is omit, resume it back
---
--- >>> defaultHrd ["a", "+56", "b"]
+-- >>> decorPlus ["a", "+56", "b"]
 -- ["+","a","+56","b"]
-defaultHrd xs@(x: _) = if isAlpha $ head x then "+":xs else xs
+decorPlus xs@(x: _) = if isVar x then "+":xs else xs
+
+omitPlus []      = []
+omitPlus ('+':t) = t
+omitPlus xs      = xs
 
 -- | first compare string's length, then compare string
 -- use Monoid style
-cmpMonomial :: String -> String -> Ordering
-cmpMonomial = (compare `on` length) `mappend` compare
+cmpVar :: String -> String -> Ordering
+cmpVar = (compare `on` length) `mappend` compare
 
-addDftOne xs = if isDigit $ last xs then xs else xs ++ "1"
-rmDftPlus [] = []
-rmDftPlus xs@(x:t) = if x == '+' then t else xs
-
--- | When only have coeff, var is void string, need resume back
---
--- >>> splitCoeffVar ["+", "a", "+56"]
--- [["+","+56"],["a",""]]
-splitCoeffVar [] = [[],[]]
-splitCoeffVar [x] = [[x], [""]]
-splitCoeffVar (x:y:xs) = if isAlpha $ head y
-                            then let ys = splitCoeffVar xs in [x: head ys, y: ys !! 1]
-                                     else let ys = splitCoeffVar (y:xs) in [x: head ys, "": ys!! 1]
-
--- | split string to poly item
---
--- * split to poly item
---
--- * sort by cmpMonomial function
---
--- * transfer coeff to Int
-splitPoly :: String -> [(Int, String)]
-splitPoly xs = sortBy (cmpMonomial `on` snd) $ zip coeff' var'
-    where [coeff, var] = splitCoeffVar $ defaultHrd $ groupBy fn xs
-          var' = map sort var
-          coeff' = map ((\x -> read x::Int) . rmDftPlus . addDftOne) coeff
-
-
-accSamePoly = foldl1 (\x y -> ((fst x) + (fst y), snd x))
-
-accPoly :: [(Int, String)] -> [(Int, String)]
-accPoly = filter (\x -> (fst x /= 0)) . map accSamePoly . groupBy ((==) `on` snd)
+readCoeff :: String -> Int
+readCoeff = read . omitPlus . addDftOne
+    where addDftOne xs = if isDigit $ last xs then xs else xs ++ "1"
 
 -- | show coeff
 -- omitt one, but need add extra plus sign
@@ -69,12 +48,33 @@ showCoeff x
   | x < 0 = show x
   | otherwise = '+': show x
 
+-- | When only have coeff, var is empty string "", need resume back
+-- >>> splitCoeffVar ["+", "a", "+56"]
+-- (["+","+56"],["a",""])
+splitCoeffVar [] = ([], [])
+splitCoeffVar [x] = ([x], [""])
+splitCoeffVar (x:y:xs) = (x: coeff, v: var)
+    where (v, ls) = if isVar y then (y, xs) else ("", y:xs)
+          (coeff, var) = splitCoeffVar ls
+
+-- | split string to poly item
+-- * split to poly item
+-- * transfer coeff to Int
+splitPoly :: String -> [(Int, String)]
+splitPoly xs = zip (map readCoeff coeff) (map sort var)
+    where (coeff, var) = splitCoeffVar $ decorPlus $ groupBy coeffOrVar xs
+
+accPoly :: [(Int, String)] -> [(Int, String)]
+accPoly = filter notO . map acc . groupBy ((==) `on` snd) . sortBy (cmpVar `on` snd)
+    where acc = foldl1 (\x y -> (((+) `on` fst) x y, snd x))
+          notO x = fst x /= 0
+
 -- ! show poly item
 -- void string case, always show first, so not add extra plus sign
 showPolyItem (a, "") = show a
-showPolyItem (a, b) = (showCoeff a) ++ b
+showPolyItem (a, b)  = showCoeff a ++ b
 -- | concat show all poly item
-showPoly = rmDftPlus . concatMap showPolyItem
+showPoly = omitPlus . concatMap showPolyItem
 
 -- | simplify multilinear polynomials
 simplify :: String -> String
