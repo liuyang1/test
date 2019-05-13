b583b11546151db0c578d037b268b16f0cd170ba
liuyang1 <liuyang1@mail.ustc.edu.cn>
2019-05-13 13:31:26 +0800

    C: std: test rand
    Signed-off-by: liuyang1 <liuyang1@mail.ustc.edu.cn>

--- lang/c/stdlib/stdlib.h/rand.c
+++ lang/c/stdlib/stdlib.h/rand.c
@@ -5,9 +5,15 @@
 int main() {
     // In the GNU C Library, it is 2147483647, which is the largest signed integer representable in
     // 32 bits. In other libraries, it may be as low as 32767.
-    printf("%s: %d\n", "RAND_MAX", RAND_MAX);
+    printf("%s: %d 0x%08X\n", "RAND_MAX", RAND_MAX, RAND_MAX);
     printf("RAND_MAX=%d ?= INT_MAX=%d %s\n",
            RAND_MAX, INT_MAX,
            RAND_MAX == INT_MAX ? "true" : "false");
+
+	// rand is SAME sequence, without seed of srand
+    int i;
+    for (i = 0; i != 10; i++) {
+        printf("i=%d %d\n", i, rand());
+    }
     return 0;
 }
