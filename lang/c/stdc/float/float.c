de78d75f375e36ce4972185fba14eb8ebe88379f
liuyang1 <liuyang1@mail.ustc.edu.cn>
2019-10-31 14:02:20 +0800

    C: float: update doc
    Signed-off-by: liuyang1 <liuyang1@mail.ustc.edu.cn>

--- lang/c/stdc/float/float.c
+++ lang/c/stdc/float/float.c
@@ -1,3 +1,21 @@
+/**
+ * Based on float IEEE.754 representation
+ *
+ * float is 32bits
+ * - 1bit: sign
+ * - 8bit: exponent
+ * - 23bit: fraction
+ *
+ * 0.15625 = (1+0.25) * 2^(-3)
+ *         = (-1)^0 * (1+0.25) * 2^(124-127)
+ *         = (-1)^sign * (1+fraction) * 2^(e-127)
+ * - sign=0
+ * - exponent=124=0b01111100
+ * - fraction=0.25=0b01000,,,
+ *
+ * so it's binary representation is 0 0111_1100 010000
+ * 0x3e200000
+ */
 #include <stdio.h>
 typedef union {
     double f;
@@ -17,6 +35,12 @@ int main() {
     T2 t2;
     t2.f = 0.15625;
     printf("%x %f %zu\n", t2.x, t2.f, sizeof(t2));
+
+    /** !!!
+     * directly
+     * printf("%x\n", 0.15625);
+     * This is wrong, printf function will do more convertion
+     */
 
     return 0;
 }
