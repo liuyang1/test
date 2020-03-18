f5275668a7a976029499138341e74aa4d674df59
liuyang1 <liuyang1@mail.ustc.edu.cn>
2020-03-18 18:46:51 +0800

    media: add qformat tool to generate factor
    Signed-off-by: liuyang1 <liuyang1@mail.ustc.edu.cn>

---
+++ mediaTool/qformat.py
@@ -0,0 +1,12 @@
+from math import *
+
+q=12
+# xs: dB, -32dB to 32dB
+xs = [i - 32 for i in range(65)]
+#xs = [-6]
+# ys: factor with q value
+ys = [round(10**(i/20.)*(2**q)) for i in xs]
+# zs: reversed exact dB value
+zs = [20*log(i/(2.0**q), 10) for i in ys]
+print(ys)
+print(zs)
