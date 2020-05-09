74f1b910ebf542fe47e0c18cf5a9e75202759bc1
liuyang1 <liuyang1@mail.ustc.edu.cn>
2020-05-09 11:13:14 +0800

    qformat: generate int instead of float
    Signed-off-by: liuyang1 <liuyang1@mail.ustc.edu.cn>

--- mediaTool/qformat.py
+++ mediaTool/qformat.py
@@ -5,7 +5,7 @@ q=12
 xs = [i - 32 for i in range(65)]
 #xs = [-6]
 # ys: factor with q value
-ys = [round(10**(i/20.)*(2**q)) for i in xs]
+ys = [int(round(10**(i/20.)*(2**q))) for i in xs]
 # zs: reversed exact dB value
 zs = [20*log(i/(2.0**q), 10) for i in ys]
 print(ys)
