ff814eaec8c1aeaf3a49d5bc0c11efe361e13eb8
liuyang1 <liuyang1@mail.ustc.edu.cn>
2021-07-08 09:31:08 +0800

    aoc2020: day2 pwd
    Signed-off-by: liuyang1 <liuyang1@mail.ustc.edu.cn>

--- topic/aoc2020/day2_pwd/src/main.rs
+++ topic/aoc2020/day2_pwd/src/main.rs
@@ -1,8 +1,9 @@
 // #[macro_use] extern crate text_io;
-use std::str::FromStr;
-use std::num::ParseIntError;
 use std::fs;
 
+/*
+use std::str::FromStr;
+use std::num::ParseIntError;
 struct Trans {
     bot: u32,
     top: u32,
@@ -13,7 +14,7 @@ struct Trans {
 impl FromStr for Trans {
     type Err = ParseIntError;
 
-    fn from_str(s: &'static str) -> Result<Self, Self::Err> {
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
         let idx0 = s.find('-').unwrap();
         let idx1 = s.find(' ').unwrap();
         let idx2 = s.find(':').unwrap();
@@ -33,14 +34,37 @@ impl FromStr for Trans {
         Ok(Trans {bot: bot, top: top, c: c, pwd: s3})
     }
 }
+*/
+
+fn from_str_2(s: &str) -> (u32, u32, char, &str) {
+    let idx0 = s.find('-').unwrap();
+    let idx1 = s.find(' ').unwrap();
+    let idx2 = s.find(':').unwrap();
+    let s0 = &s[..idx0];
+    let s1 = &s[(idx0+1)..idx1];
+    let s2 = &s[(idx1+1)..(idx1+2)];
+    let s3 = &s[(idx2+2)..];
+    let bot = s0.parse::<u32>().unwrap();
+    let top = s1.parse::<u32>().unwrap();
+    let c = s2.chars().nth(0).unwrap();
+    (bot, top, c, s3)
+}
+
+fn char_count(s: &str, c: char) -> u32 {
+    s.chars().map(|i| if i == c {1} else {0}).sum()
+}
 
 fn main() {
-    let ts = Trans::from_str("12-24 a: adefbc").unwrap();
-    println!("{} {} {} {}", ts.bot, ts.top, ts.c, ts.pwd);
-    /*
+    // let ts = Trans::from_str("12-24 a: adefbc").unwrap();
+    // println!("{} {} {} {}", ts.bot, ts.top, ts.c, ts.pwd);
+
     let contents = fs::read_to_string("input.0").unwrap();
-    let xs = contents.lines()
-        .collect::<Vec<&str>>();
-    println!("{:?}", xs);
-    */
+    let xs = contents.lines().collect::<Vec<&str>>();
+    // println!("{:?}", xs);
+    for i in xs {
+        let (bot, top, c, pwd) = from_str_2(i);
+        let cnt = char_count(pwd, c);
+        let res = if bot <= cnt && cnt <= top { "valid  " } else{ "invalid"};
+        println!("{} {} {} {} {}", res, bot, top, c, pwd);
+    }
 }
