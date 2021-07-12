89f5f5f3ae01855e927a8271c7e8e92bb853db0c
liuyang1 <liuyang1@mail.ustc.edu.cn>
2021-07-12 10:47:26 +0800

    aoc2020: refine day6
    Signed-off-by: liuyang1 <liuyang1@mail.ustc.edu.cn>

--- topic/aoc2020/custom_day6/src/main.rs
+++ topic/aoc2020/custom_day6/src/main.rs
@@ -12,42 +12,24 @@ fn counter_char(s :&str) -> [u32; 26] {
     a
 }
 
+fn counter_pred(a :[u32;26], pred: impl Fn(u32) -> bool) -> usize {
+    a.iter().map(|i| if pred(*i) {1} else {0}).sum()
+}
+
 fn counter(s :&str) -> usize {
-    let a = counter_char(s);
-    let mut sum = 0;
-    for cnt in a {
-        if cnt != 0 {
-            sum = sum + 1;
-        }
-    }
-    sum
+    counter_pred(counter_char(s), |i| i != 0)
 }
 
 fn counter_every(s :&str) -> usize {
     let xs = s.split_whitespace().collect::<Vec<&str>>();
-    let a = counter_char(s);
     let ln :u32 = xs.len() as u32;
-    let mut sum = 0;
-    for cnt in a {
-        if cnt == ln {
-            sum = sum + 1;
-        }
-    }
-    println!("{:?} {}", xs, sum);
-    sum
+    counter_pred(counter_char(s), |i| i == ln)
 }
 
 fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
     let xs = contents.split("\n\n").collect::<Vec<&str>>();
-    let mut cnt = 0;
-    // for s in xs {
-    //     cnt = cnt + counter(s);
-    // }
-    // println!("{}", cnt); // 7027
-    cnt = 0;
-    for s in xs {
-        cnt = cnt + counter_every(s);
-    }
-    println!("{}", cnt); // 3579
+    let cnt0:usize = xs.iter().map(|s| counter(s)).sum();
+    let cnt1:usize = xs.iter().map(|s| counter_every(s)).sum();
+    println!("{} {}", cnt0, cnt1); // 7027 3579
 }
